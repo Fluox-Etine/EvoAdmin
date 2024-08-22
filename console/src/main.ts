032f26527e3a669ae054cb9414077acc23d6698b
故人重来 <1820392627@qq.com>
2024-08-22 20:29:21 +0800

    feta: 安装引入unocss插件解决样式问题

--- console/src/main.ts
+++ console/src/main.ts
@@ -4,6 +4,7 @@ import Antd from 'ant-design-vue';
 import App from './App.vue'
 import { setupRouter } from './router';
 import {setupStore} from '@/store';
+import 'virtual:uno.css'
 import 'ant-design-vue/dist/reset.css';
 
 const app = createApp(App)
--- console/vite.config.ts
+++ console/vite.config.ts
@@ -1,13 +1,13 @@
-import {fileURLToPath, URL} from 'node:url'
-
-import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {resolve} from "path";
+import Unocss from 'unocss/vite';
+import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
+        Unocss(),
     ],
     resolve: {
         alias: {
