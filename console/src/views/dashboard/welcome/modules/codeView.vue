5a78eb9ef18f704891fae59e298f2ec7a8708d67
故人重来 <1820392627@qq.com>
2024-08-25 20:24:12 +0800

    feat(console): 集成Highlight.js进行代码高亮
    在控制台应用中集成了Highlight.js库，以启用代码高亮功能。特别添加了对PHP语言的支持，并应用了a11y-dark样式主题以提升用户体验。

---
+++ console/src/views/dashboard/welcome/modules/codeView.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <a-alert
+        message="温馨提示（代码预览）"
+        description="温馨提示，现在还没有什么提示"
+        type="success"
+        show-icon
+    />
+    <br>
+    <a-card
+        style="width: 100%;margin-bottom: 20px"
+        :tab-list="tabCodeList"
+        :active-tab-key="codeKey"
+        @tabChange="key => onCodeTabChange(key)"
+    >
+      <div v-show="codeKey === 'controller'" style="width: 100%;">
+        <highlightjs v-if="code.controller" language="php" :code="code.controller"/>
+        <a-empty v-else/>
+      </div>
+      <div v-show="codeKey === 'logic'" style="width: 100%;">
+        <highlightjs v-if="code.logic" language="php" :code="code.logic"/>
+        <a-empty v-else/>
+      </div>
+      <div v-show="codeKey === 'model'" style="width: 100%;">
+        <highlightjs v-if="code.model" language="php" :code="code.model"/>
+        <a-empty v-else/>
+      </div>
+      <div v-show="codeKey === 'validate'" style="width: 100%;">
+        <a-empty/>
+      </div>
+    </a-card>
+  </div>
+</template>
+<script lang="ts" setup>
+import {ref} from "vue";
+
+defineProps<{ code: object }>();
+
+const tabCodeList = [
+  {
+    key: 'controller',
+    tab: '控制器'
+  },
+  {
+    key: 'logic',
+    tab: '逻辑层'
+  },
+  {
+    key: 'model',
+    tab: '模型层'
+  },
+  {
+    key: 'validate',
+    tab: '验证器'
+  }
+];
+
+const codeKey = ref('controller');
+
+
+/** 切换tab */
+const onCodeTabChange = (value: string) => {
+  codeKey.value = value;
+};
+</script>
+<style scoped lang="less">
+
+</style>
