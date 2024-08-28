320773a493967d433cfe171ff3b57afd0eab9bc3
Fluoxetine <1820392627@qq.com>
2024-08-28 17:19:41 +0800

    feat:准备下班，回家继续撸这部分代码

--- console/src/router/routes/modules/dashboard.ts
+++ console/src/router/routes/modules/dashboard.ts
@@ -1,28 +1,35 @@
-import type { RouteRecordRaw } from 'vue-router';
-
-const moduleName = 'dashboard';
+import type {RouteRecordRaw} from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/dashboard',
-    name: moduleName,
-    redirect: '/dashboard/welcome',
-    meta: {
-      title: '控制台',
-      icon: 'ant-design:dashboard-outlined',
-    },
-    children: [
-      {
-        path: 'welcome',
-        name: `${moduleName}-welcome`,
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        redirect: '/dashboard/welcome',
         meta: {
-          title: '仪表台',
-          icon: 'ant-design:home-filled',
+            title: '控制台',
+            icon: 'ant-design:dashboard-outlined',
         },
-        component: () => import('@/views/dashboard/welcome/index.vue'),
-      },
-    ],
-  },
+        children: [
+            {
+                path: 'welcome',
+                name: `dashboard-welcome`,
+                meta: {
+                    title: '仪表台',
+                    icon: 'ant-design:home-filled',
+                },
+                component: () => import('@/views/dashboard/welcome/index.vue'),
+            },
+            {
+                path: '/demos-table',
+                name: `demos-table`,
+                meta: {
+                    title: '数据表格',
+                    icon: 'ant-design:desktop-outlined',
+                },
+                component: () => import('@/views/dashboard/demos/table.vue'),
+            }
+        ],
+    }
 ];
 
 export default routes;
