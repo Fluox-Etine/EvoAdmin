650a89d2889f3c99b6a6b92017c1f0d05c07732b
Fluoxetine <1820392627@qq.com>
2024-08-30 12:01:49 +0800

    refactor(admin): 简化上传文件逻辑并更新异常处理简化UploadController中的upload方法，移除try-catch块并添加RespBusinessException异常。更新异常处理逻辑，以提供更具体的错误信息。refactor(vue): 创建vue API请求和服务的类型及函数存根
    在Vue项目中添加request和types存根文件，以提供API请求和服务的类型及函数定义。这将有助于自动完成和错误检查。

--- server/app/http/admin/controller/common/UploadController.php
+++ server/app/http/admin/controller/common/UploadController.php
@@ -4,6 +4,7 @@ namespace app\http\admin\controller\common;
 
 use app\common\logic\UploadLogic;
 use app\http\admin\service\system\SysAdminService;
+use support\exception\RespBusinessException;
 use support\Request;
 use support\Response;
 
@@ -13,20 +14,19 @@ class UploadController
      * 文件上传
      * @param Request $request
      * @return Response
+     * @throws RespBusinessException
      */
     public function upload(Request $request): Response
     {
-        try {
-            $file = $request->file('file');
-            $result = UploadLogic::handleUpload(
-                $file,
-                SysAdminService::getCurrentLoginId(),
-                $request->post('group', 0),
-                10, $request->post('type', 10)
-            );
-            return renderSuccess($result);
-        } catch (\Exception $e) {
-            return renderError($e->getMessage());
-        }
+
+        $file = $request->file('file');
+        $result = UploadLogic::handleUpload(
+            $file,
+            SysAdminService::getCurrentLoginId(),
+            $request->post('group', 0),
+            10, $request->post('type', 10)
+        );
+        return renderSuccess($result);
+
     }
 }
