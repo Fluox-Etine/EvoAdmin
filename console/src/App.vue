9b95a69ff807af4924d0dac1196f7572f7e8c497
故人重来 <1820392627@qq.com>
2024-09-09 20:14:13 +0800

    fix:修复水印组件，在没有用户信息白屏幕bug

--- console/src/App.vue
+++ console/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <ProConfigProvider>
-    <a-watermark :content="userStore.userInfo.username" v-show="userStore.userInfo.username" :zIndex="9999999" :gap="[75,75]">
+    <a-watermark :content="userStore.userInfo.username" v-show="userStore.userInfo" :zIndex="9999999"
+                 :gap="[75,75]">
       <router-view #="{ Component }">
         <component :is="Component"/>
       </router-view>
@@ -13,6 +14,6 @@ import ProConfigProvider from "@/components/core/pro-config-provider/index.vue";
 import {useUserStore} from "@/store/modules/user.ts";
 
 const userStore = useUserStore()
-console.log(userStore.userInfo.username)
+console.log(userStore)
 
 </script>
